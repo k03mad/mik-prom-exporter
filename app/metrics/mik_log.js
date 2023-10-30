7c1bae0d87353eea7402c9da96ab9b0d1239f142
k03mad <k03.mad@gmail.com>
2023-10-30 15:45:11 +0300

    remove dns redir

--- app/metrics/mik_log.js
+++ app/metrics/mik_log.js
@@ -2,10 +2,6 @@ import Mikrotik from '../api/mikrotik.js';
 import {countDupsBy} from '../helpers/object.js';
 import {getCurrentFilename} from '../helpers/paths.js';
 
-const REDIRECT_DNS_DEST_RE = /dstnat.+->([\d.]+):53/;
-const REDIRECT_DNS_SRC_RE = /dstnat.+ ([\d.]+):\d+/;
-const REDIRECT_DNS_PROTO_RE = /dstnat.+proto (\w+).+->.+:53/;
-
 export default {
     name: getCurrentFilename(import.meta.url),
     help: 'log',
@@ -14,71 +10,17 @@ export default {
     async collect(ctx) {
         ctx.reset();
 
-        const [
-            log,
-            ipDhcpServerLeaseToName,
-        ] = await Promise.all([
-            Mikrotik.log(),
-            Mikrotik.ipDhcpServerLeaseToName(),
-        ]);
+        const log = await Mikrotik.log();
 
         const topics = {};
-        const redirectDnsDest = {};
-        const redirectDnsSrc = {};
-        const redirectDnsProto = {};
-        const redirectDnsFull = {};
 
         log.forEach((item, i) => {
             ctx.labels('entries', item.time, item.topics, item.message, null).set(++i);
             countDupsBy(item.topics, topics);
-
-            const redirectFull = [];
-
-            const srcDns = item.message.match(REDIRECT_DNS_SRC_RE)?.[1];
-
-            if (srcDns) {
-                const nameOrIp = ipDhcpServerLeaseToName[srcDns] || srcDns;
-                countDupsBy(nameOrIp, redirectDnsSrc);
-                redirectFull.push(nameOrIp);
-            }
-
-            const destDns = item.message.match(REDIRECT_DNS_DEST_RE)?.[1];
-
-            if (destDns) {
-                countDupsBy(destDns, redirectDnsDest);
-                redirectFull.push('=>', destDns);
-            }
-
-            const protoDns = item.message.match(REDIRECT_DNS_PROTO_RE)?.[1];
-
-            if (protoDns) {
-                countDupsBy(protoDns, redirectDnsProto);
-                redirectFull.push(`(${protoDns})`);
-            }
-
-            if (redirectFull.length > 0) {
-                countDupsBy(redirectFull.join(' '), redirectDnsFull);
-            }
         });
 
         Object.entries(topics).forEach(([key, value]) => {
             ctx.labels('topics', null, null, null, key).set(value);
-        });
-
-        Object.entries(redirectDnsDest).forEach(([key, value]) => {
-            ctx.labels('redirect-dns-dest', null, null, null, key).set(value);
-        });
-
-        Object.entries(redirectDnsSrc).forEach(([key, value]) => {
-            ctx.labels('redirect-dns-src', null, null, null, key).set(value);
-        });
-
-        Object.entries(redirectDnsProto).forEach(([key, value]) => {
-            ctx.labels('redirect-dns-proto', null, null, null, key).set(value);
-        });
-
-        Object.entries(redirectDnsFull).forEach(([key, value]) => {
-            ctx.labels('redirect-dns-full', null, null, null, key).set(value);
         });
     },
 };
