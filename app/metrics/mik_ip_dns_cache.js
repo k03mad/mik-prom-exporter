b40d1447dd8ac1547d7cac2fe9e7ed302532e5d9
k03mad <k03.mad@gmail.com>
2024-02-11 01:11:29 +0300

    add domains to vpn log

--- app/metrics/mik_ip_dns_cache.js
+++ app/metrics/mik_ip_dns_cache.js
@@ -1,6 +1,15 @@
+import env from '../../env.js';
 import Mikrotik from '../api/mikrotik.js';
 import {countDupsBy} from '../helpers/object.js';
 import {getCurrentFilename} from '../helpers/paths.js';
+
+const DOMAINS_INCLUDES_TOVPN_LIST = [
+    'facebook',
+    'fbcdn',
+    'instagram',
+    'twimg',
+    'twitter',
+];
 
 export default {
     name: getCurrentFilename(import.meta.url),
@@ -10,16 +19,36 @@ export default {
     async collect(ctx) {
         ctx.reset();
 
-        const ipDnsCache = await Mikrotik.ipDnsCache();
+        const [
+            ipFirewallAddressList,
+            ipDnsCache,
+        ] = await Promise.all([
+            Mikrotik.ipFirewallAddressList(),
+            Mikrotik.ipDnsCache(),
+        ]);
 
         const dnsCacheTypes = {};
+        const dnsCacheDomainsToVpn = {};
 
         ipDnsCache.forEach(elem => {
             elem.type && countDupsBy(elem.type, dnsCacheTypes);
         });
 
+        ipDnsCache.forEach(elem => {
+            if (
+                DOMAINS_INCLUDES_TOVPN_LIST.some(domain => elem.name.includes(domain))
+                && !ipFirewallAddressList.some(list => list.list === env.mikrotik.toVpnList && list.address === elem.name)
+            ) {
+                countDupsBy(elem.name, dnsCacheDomainsToVpn);
+            }
+        });
+
         Object.entries(dnsCacheTypes).forEach(([key, value]) => {
             ctx.labels('records', key).set(value);
+        });
+
+        Object.entries(dnsCacheDomainsToVpn).forEach(([key, value]) => {
+            ctx.labels('domainsToVpn', key).set(value);
         });
     },
 };
